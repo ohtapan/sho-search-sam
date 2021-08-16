00239e813f9a26a5c927df80be92166cb2a3c9d3
HarutoOta <a2011505@sendai-nct.jp>
2021-08-16 23:09:50 +0900

    ver1.0

--- sho-search-lambda/app.py
+++ sho-search-lambda/app.py
@@ -33,5 +33,10 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
         "body": json.dumps(sorted_results, indent=2, ensure_ascii=False),
     }
